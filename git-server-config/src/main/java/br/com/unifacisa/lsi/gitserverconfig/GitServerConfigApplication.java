ebc364e3a56123d747f8b042639ea4a105e648a8
Helder Barbosa <helder.barbosa@ctis.com.br>
2019-11-27 18:47:58 -0300

    configurando

--- git-server-config/src/main/java/br/com/unifacisa/lsi/gitserverconfig/GitServerConfigApplication.java
+++ git-server-config/src/main/java/br/com/unifacisa/lsi/gitserverconfig/GitServerConfigApplication.java
@@ -2,8 +2,10 @@ package br.com.unifacisa.lsi.gitserverconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class GitServerConfigApplication {
 
 	public static void main(String[] args) {
